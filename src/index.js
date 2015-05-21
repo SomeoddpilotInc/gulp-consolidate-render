9ac527058bc3db86eb23bbf89e9b64b97e8ba863
Alex Robertson <alex@someoddpilot.com>
2015-05-21 09:52:19 -0500

    Using promises to flatten pyramid of doom

--- src/index.js
+++ src/index.js
@@ -8,6 +8,29 @@ function basicCompileData(sources) {
   return _.merge.apply(_, sources);
 }
 
+function templateFileExists(templatePath) {
+  return new Promise(function promisifyExists(resolve, reject) {
+    fs.exists(templatePath, function onExists(exists) {
+      if (!exists) {
+        return reject(new Error("Template does not exist at " + templatePath));
+      }
+      resolve(true);
+    });
+  });
+}
+
+function consolidateP(engine, templatePath, data) {
+  return new Promise(function (resolve, reject) {
+    consolidate[engine](templatePath, data, function (err, html) {
+      if (err) {
+        return reject(err);
+      }
+
+      resolve(html);
+    });
+  });
+}
+
 function templates(options) {
   var compiledOptions = _.merge({
     templateDir: "templates",
@@ -36,31 +59,28 @@ function templates(options) {
       templateName + compiledOptions.extension
     );
 
-    fs.exists(templatePath, function onExists(exists) {
-      if (!exists) {
-        throw new Error("Template does not exist at " + templatePath);
-      }
+    templateFileExists(templatePath)
+      .then(function () {
+        var data = compileData([
+          {},
+          globals,
+          file.frontMatter || {},
+          {
+            contents: file.contents.toString()
+          },
+          file
+        ]);
 
-      var data = compileData([
-        {},
-        globals,
-        file.frontMatter || {},
-        {
-          contents: file.contents.toString()
-        },
-        file
-      ]);
-
-      consolidate[compiledOptions.engine](templatePath, data, function onRender(err, html) {
-        if (err) {
-          throw err;
-        }
-
+        return consolidateP(compiledOptions.engine, templatePath, data);
+      })
+      .then(function (html) {
         file.contents = new Buffer(html, "utf-8");
 
         callback(null, file);
+      })
+      .catch(function (err) {
+        throw err;
       });
-    });
   });
 }
 
